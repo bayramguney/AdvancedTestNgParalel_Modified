89899330717a02e38213e07fdf8f42d341583113
bayramguney <56900075+bayramguney@users.noreply.github.com>
2023-04-19 18:02:03 -0400

    Update TestCase.java

--- src/test/java/Tests/TestCase.java
+++ src/test/java/Tests/TestCase.java
@@ -1,39 +1,41 @@
 package Tests;
 
-import java.util.HashMap;
 
-import org.testng.*;
-import org.testng.annotations.DataProvider;
+import java.util.Hashtable;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.HomePageObjects;
-import reusableComponents.ExcelOperations;
-import testBase.*;
-
-/**
- * @author: Prakash Narkhede
- * @Youtube: https://www.youtube.com/automationtalks
- * @LinkedIn: https://www.linkedin.com/in/panarkhede89/
- */
-
-public class TestCase extends TestBase{
-
-	HomePageObjects homePage = new HomePageObjects();
+import reusableComponents.DataProviders;
+import testBase.DriverFactory;
+import testBase.ExtentFactory;
+import testBase.TestBase;
 
 
+public class TestCase extends TestBase {
 
-	
-	@Test()
-	public void TaskCreationTest() throws Throwable {
-
-
-		ExtentFactory.getInstance().getExtent().info("Test Data for this execution run is: "+new Throwable().getStackTrace()[0].getMethodName());
-
-      homePage.search();
-      homePage.validatetitle();
+    HomePageObjects homePage = new HomePageObjects();
 
 
-	}
+    @Test(dataProviderClass = DataProviders.class, dataProvider = "dp", priority = 1)
+    public void TaskCreationTest(Hashtable<String,String> data) throws Throwable {
+
+
+        ExtentFactory.getInstance().getExtent().info("Test Data for this execution run is: " + new Throwable().getStackTrace()[0].getMethodName());
+        System.out.println(data.get("name")+"---"+data.get("lastname"));
+        homePage.search();
+        homePage.validatetitle();
+
+        String google=DriverFactory.getInstance().getDriver().getWindowHandle();
+
+        Assert.fail();
+        openNewTab("https://www.wikipedia.org/");
+
+        String wiki=DriverFactory.getInstance().getDriver().getWindowHandle();
+
+
+        DriverFactory.getInstance().getDriver().switchTo().window(google);
 
 
 
+    }
 }
