3233c2c6e9ab82c0acfdf032f57b99602866ddc0
bayramguney <56900075+bayramguney@users.noreply.github.com>
2023-04-19 14:27:05 -0400

    Update ListenersImplementation.java

--- src/main/java/reusableComponents/ListenersImplementation.java
+++ src/main/java/reusableComponents/ListenersImplementation.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import org.apache.commons.io.FileUtils;
-import org.apache.http.client.ClientProtocolException;
-import org.json.simple.parser.ParseException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
@@ -22,79 +23,80 @@ import testBase.DriverFactory;
 import testBase.ExtentFactory;
 import testBase.ExtentReportNG;
 
-/**
- * @author: Prakash Narkhede
- * @Youtube: https://www.youtube.com/automationtalks
- * @LinkedIn: https://www.linkedin.com/in/panarkhede89/
- */
-public class ListenersImplementation implements ITestListener{
 
-	static ExtentReports report;
-		   ExtentTest test;
-		   
-	public void onTestStart(ITestResult result) {
-		//before each test case
-		test = report.createTest(result.getMethod().getMethodName());
-		ExtentFactory.getInstance().setExtent(test);
-	}
+public class ListenersImplementation implements ITestListener {
 
-	public void onTestSuccess(ITestResult result) {
-		ExtentFactory.getInstance().getExtent().log(Status.PASS, "Test Case: "+result.getMethod().getMethodName()+ " is Passed.");
-		ExtentFactory.getInstance().removeExtentObject();
-	}
+    static ExtentReports report;
+    ExtentTest test;
 
-	public void onTestFailure(ITestResult result) {
-		ExtentFactory.getInstance().getExtent().log(Status.FAIL, "Test Case: "+result.getMethod().getMethodName()+ " is Failed.");
-		ExtentFactory.getInstance().getExtent().log(Status.FAIL, result.getThrowable());
-		
-		//add screenshot for failed test.
-		File src = ((TakesScreenshot)DriverFactory.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
-		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyy HH-mm-ss");
-		Date date = new Date();
-		String actualDate = format.format(date);
-		
-		String screenshotPath = System.getProperty("user.dir")+
-				"/Reports/Screenshots/"+actualDate+".jpeg";
-		File dest = new File(screenshotPath);
-		
-		try {
-			FileUtils.copyFile(src, dest);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			ExtentFactory.getInstance().getExtent().addScreenCaptureFromPath(screenshotPath, "Test case failure screenshot");
-			ExtentFactory.getInstance().removeExtentObject();
+    public void onTestStart(ITestResult result) {
+        //before each test case
+        test = report.createTest(result.getMethod().getRealClass().getName());
+        ExtentFactory.getInstance().setExtent(test);
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public void onTestSuccess(ITestResult result) {
+        String text = "<b>Test Case: " + result.getMethod().getRealClass().getName() + "==>" + result.getMethod().getMethodName() + " is Passed.</b>";
+        Markup m = MarkupHelper.createLabel(text, ExtentColor.GREEN);
+        ExtentFactory.getInstance().getExtent().log(Status.PASS,m);
+        ExtentFactory.getInstance().removeExtentObject();
+    }
 
+    public void onTestFailure(ITestResult result) {
+        String text="<b>Test Case: " + result.getMethod().getMethodName() + " is Failed.</b>";
+        Markup m = MarkupHelper.createLabel(text, ExtentColor.RED);
+        ExtentFactory.getInstance().getExtent().log(Status.FAIL,m);
+        ExtentFactory.getInstance().getExtent().log(Status.FAIL, result.getThrowable());
 
-	}
+        //add screenshot for failed test.
+        File src = ((TakesScreenshot) DriverFactory.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyy HH-mm-ss");
+        Date date = new Date();
+        String actualDate = format.format(date);
 
-	public void onTestSkipped(ITestResult result) {
-		ExtentFactory.getInstance().getExtent().log(Status.SKIP, "Test Case: "+result.getMethod().getMethodName()+ " is skipped.");
-		ExtentFactory.getInstance().removeExtentObject();
-	}
+        String screenshotPath = System.getProperty("user.dir") +
+                "/Reports/Screenshots/" + actualDate + ".jpeg";
+        File dest = new File(screenshotPath);
 
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-	}
+        try {
+            FileUtils.copyFile(src, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            ExtentFactory.getInstance().getExtent().addScreenCaptureFromPath(screenshotPath, "Test case failure screenshot");
+            ExtentFactory.getInstance().removeExtentObject();
 
-	public void onTestFailedWithTimeout(ITestResult result) {
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	public void onStart(ITestContext context) {
-		try {
-			 report = ExtentReportNG.setupExtentReport();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
-	public void onFinish(ITestContext context) {
-		//close extent
-		report.flush();
-	}
+    public void onTestSkipped(ITestResult result) {
+        String text="<b>Test Case: " + result.getMethod().getMethodName() + " is skipped.</b>";
+        Markup m = MarkupHelper.createLabel(text, ExtentColor.ORANGE);
+        ExtentFactory.getInstance().getExtent().log(Status.SKIP, m);
+        ExtentFactory.getInstance().removeExtentObject();
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    }
+
+    public void onTestFailedWithTimeout(ITestResult result) {
+    }
+
+    public void onStart(ITestContext context) {
+        try {
+            report = ExtentReportNG.setupExtentReport();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onFinish(ITestContext context) {
+        //close extent
+        report.flush();
+    }
 
 }
