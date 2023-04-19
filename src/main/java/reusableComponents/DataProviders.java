15ec1b157d042eedcb2a3ea5340d0ce0decc93c1
bayramguney <56900075+bayramguney@users.noreply.github.com>
2023-04-19 17:58:46 -0400

    Create DataProviders.java

---
+++ src/main/java/reusableComponents/DataProviders.java
@@ -0,0 +1,30 @@
+package reusableComponents;
+
+
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+
+
+
+    @DataProvider(name="dp")
+    public static Object[][] getData() {
+
+        ExcelReader excel = new ExcelReader(System.getProperty("user.dir") + PropertiesOperations.getPropertyValueByKey("testDataLocation"));
+        return DataUtil.getData(excel,PropertiesOperations.getPropertyValueByKey("sheetName"));
+    }
+
+    @DataProvider(name="dp1")
+    public static Object[][] getUserName() {
+
+        ExcelReader excel = new ExcelReader(System.getProperty("user.dir") + "\\src\\test\\resources\\testdata\\UserData.xlsx");
+        String sheetName="UserName";
+
+        return DataUtil.getData(excel,sheetName);
+    }
+
+
+
+
+}
